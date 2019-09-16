88d115ba905d58dfafcebab181976881beaef32e
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-16 11:42:49 -0300

    import components Button and ShowLap
    Showing, incrementing and decrementing number of turns

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Button from './Components/Button';
+import ShowLap from './Components/ShowLap';
 import './App.css';
 
 function App() {
+  const [numLaps, setNumLaps] = useState(0);
+  
+  const increment = () => {
+    setNumLaps(numLaps + 1)
+  }
+
+  const decrement = () => {
+    setNumLaps(numLaps - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShowLap laps={numLaps} />
+      <Button onClick={increment}>+</Button>
+      <Button onClick={decrement}>-</Button>
     </div>
   );
 }
