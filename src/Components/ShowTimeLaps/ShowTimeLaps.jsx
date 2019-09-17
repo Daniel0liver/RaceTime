523b53755be04cfb1663e7db0ffce3a289ec0df1
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-17 09:25:58 -0300

    Create component ShowTimeLaps
    The ShowTimeLaps component will be responsible for Showing lap times at each increment / decrease of laps.

---
+++ src/Components/ShowTimeLaps/ShowTimeLaps.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ShowTimeLaps.css';
+
+export default (props) => {
+  const time = props.timeLaps;
+  const minutes = parseInt(time/60)
+  const seconds = time % 60;
+  const minutesStr = minutes < 10 ? '0' + minutes : minutes;
+  const secondsStr = seconds < 10 ? '0' + seconds : seconds;
+
+  return(
+    <div className="footer">
+      <h1>
+        {`${minutesStr}:${secondsStr}`}
+      </h1>
+      <p>Tempo médio por voltas</p>
+    </div>
+  )
+}
