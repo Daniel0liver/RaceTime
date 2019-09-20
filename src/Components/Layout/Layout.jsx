4ffa91820ed75cabe3e6d94b40211f07bb6d1bda
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-19 22:48:30 -0300

    Update
    font-size, new font-family and
    corrections in width and height

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { 
-    BrowserRouter as Router, 
-    Route,
-    Link 
-  } from 'react-router-dom';
-
 import Container from '../Container';
+
 import './Layout.css';
 
-export default ({ children }) => {
-   return(
-      <div>
-         <Container>
-            { children }
-         </Container>
-      </div>
-   )
+export default  ({ children }) => {
+  return (
+    <Container>
+      {children}
+    </Container>
+  )
 }
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 
-
-export default (props) => {
+export default (props, { children }) => {
   return(
     <div className="sidebar menu">
       <ul>
