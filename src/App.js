61b23d057e8503e1d9d02d66e1757c23e1debc3c
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-19 22:46:08 -0300

    Remove Component Button

--- src/App.js
+++ src/App.js
@@ -4,42 +4,33 @@ import {
   Route,
   Link 
 } from 'react-router-dom';
-
-import Container from './Components/Container';
-import Button from './Components/Button';
+import Layout from './Components/Layout';
 import Sidebar from './Components/Sidebar';
 
 import StopWatch from './Pages/StopWatch';
 import Run from './Pages/Run';
-import Layout from './Components/Layout';
 
 import './App.css';
 
 function App() {
-
   return (
     <Layout>
-      <Router>
-        <Sidebar>
-          <Button>
-              <Link to="/Run" >
-                <i class="material-icons">access_alarms</i>
-              </Link>
-          </Button>
-          <Button>
-          <Link to="/StopWatch">
-              <i  class="material-icons">add_alarm</i>
-          </Link>
-          </Button>
-          <Button>
-                <i class="material-icons">timer</i>
-          </Button>
-        </Sidebar>
-        <Route path="/Run" component={Run} />
-        <Route path="/StopWatch" component={StopWatch} />
-      </Router>
-    </Layout>
-      
+    <Router>
+      <Sidebar>
+        <Link to="/Run" >
+          <i class="material-icons">directions_run</i>
+        </Link>
+        <Link to="/StopWatch">
+            <i  class="material-icons">timer</i>
+        </Link>
+        <Link>
+          <i class="material-icons">add_alarm</i>
+        </Link>
+      </Sidebar>
+      <Route path="/Run" component={Run} />
+      <Route path="/StopWatch" component={StopWatch} />
+    </Router>
+    </Layout>      
   );
 }
 
