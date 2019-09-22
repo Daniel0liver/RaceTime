2f3d92f7f37d3eb379954f981ceb57dbdc7c7194
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-22 11:47:54 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { 
   BrowserRouter as Router, 
   Route,
   Link 
 } from 'react-router-dom';
-import Layout from './Components/Layout';
 import Sidebar from './Components/Sidebar';
 
 import StopWatch from './Pages/StopWatch';
 import Run from './Pages/Run';
+import HomePage from './Pages/HomePage';
 
 import './App.css';
 
 function App() {
   return (
-    <Layout>
-    <Router>
-      <Sidebar>
-        <Link to="/Run" >
-          <i class="material-icons">directions_run</i>
-        </Link>
-        <Link to="/StopWatch">
-            <i  class="material-icons">timer</i>
-        </Link>
-        <Link>
-          <i class="material-icons">add_alarm</i>
-        </Link>
-      </Sidebar>
-      <Route path="/Run" component={Run} />
-      <Route path="/StopWatch" component={StopWatch} />
-    </Router>
-    </Layout>      
+      <Router>
+        <Sidebar>
+          <Link to="/">
+            <i className="material-icons">home</i>
+          </Link>
+          <Link to="/Run" >
+            <i className="material-icons">directions_run</i>
+          </Link>
+          <Link to="/StopWatch">
+              <i  className="material-icons">timer</i>
+          </Link>
+        </Sidebar>
+        <Route path="/" component={HomePage} />
+        <Route path="/Run" component={Run} />
+        <Route path="/StopWatch" component={StopWatch} />
+      </Router>    
   );
 }
 
