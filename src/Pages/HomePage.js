7179e150b3d255903d1539b268be26f93bc6acad
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-09-22 11:47:01 -0300

    Create HomePage and add component Animation

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../Components/Layout';
+import Animation from '../Components/Animation'
+
+export default () => {
+	return(
+    <Layout>
+        <h1 className="title">Home</h1>
+        <Animation />
+    </Layout>
+	)
+}
